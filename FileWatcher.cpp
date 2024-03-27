d4a3a5fca66316f4728ad1d42d35f1bf23a94e9e
diego_jgb <diegojose78@gmail.com>
2024-03-27 04:08:03 -0400

    Removed storing of match in regex search

--- FileWatcher.cpp
+++ FileWatcher.cpp
@@ -44,7 +44,6 @@ void FileWatcher::onFileChanged(const QString &path)
 
     if (monitor->m_file.is_open()) {
         std::string line;
-        std::smatch match;
         bool enabled = true;
 
         if (!std::getline(monitor->m_file, line)) { // In case weird modifications were made, and the cursor is broken.
@@ -54,7 +53,7 @@ void FileWatcher::onFileChanged(const QString &path)
 
         do {
             for (const Notifier* notifier: monitor->m_enabledNotifiers) {
-                if (enabled && std::regex_search(line, match, notifier->regex)) {
+                if (enabled && std::regex_search(line, notifier->regex)) {
                     if (notifier->toastEnabled())
                         emit matchFound(notifier->templ);
                     else {
@@ -76,7 +75,6 @@ void FileWatcher::onFileChanged(const QString &path)
 
                         mciSendString(playCommand.c_str(), NULL, 0, NULL);
                     }
-                    qDebug() << "Found: " << match.str();
                     enabled = false;
                 }
             }
