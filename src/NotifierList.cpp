f96e03427897a76f0fbb1df10c5d9ec2d0e039ca
diego_jgb <diegojose78@gmail.com>
2024-04-19 16:52:14 -0400

    Added NotifierList class

---
+++ src/NotifierList.cpp
@@ -0,0 +1,50 @@
+#include "NotifierList.h"
+
+NotifierList::NotifierList(QObject *parent)
+    : QAbstractListModel{parent}
+{}
+
+int NotifierList::rowCount(const QModelIndex& parent) const
+{
+    return m_notifiers.size();
+}
+
+QVariant NotifierList::data(const QModelIndex& index, int role) const
+{
+    if (!index.isValid() || index.row() >= m_notifiers.size())
+        return QVariant();
+
+    if(role == Qt::DisplayRole) {
+        qDebug() << m_notifiers[index.row()]->name();
+        return QVariant::fromValue(m_notifiers[index.row()]->name());
+    }
+
+    return QVariant();
+}
+
+Notifier* NotifierList::at(int i) const
+{
+    if (i < 0 || i >= m_notifiers.size())
+        return nullptr;
+
+    return m_notifiers[i];
+}
+
+void NotifierList::append(Notifier* notifier)
+{
+    int newIndex = m_notifiers.size();
+
+    beginInsertRows(QModelIndex(), newIndex, newIndex);
+    m_notifiers.append(notifier);
+    endInsertRows();
+}
+
+void NotifierList::remove(int i)
+{
+    if (i < 0 || i >= m_notifiers.size())
+        return;
+
+    beginRemoveRows(QModelIndex(), i, i);
+    m_notifiers.removeAt(i);
+    endRemoveRows();
+}
---
+++ src/NotifierList.h
@@ -0,0 +1,28 @@
+#ifndef NOTIFIERLIST_H
+#define NOTIFIERLIST_H
+
+#include "Notifier.h"
+
+#include <QObject>
+#include <QAbstractListModel>
+
+class NotifierList : public QAbstractListModel
+{
+    Q_OBJECT
+
+public:
+    explicit NotifierList(QObject *parent = nullptr);
+
+    int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+
+public slots:
+    void append(Notifier* notifier);
+    void remove(int i);
+    Notifier* at(int i) const;
+
+private:
+    QList<Notifier*> m_notifiers;
+};
+
+#endif // NOTIFIERLIST_H
