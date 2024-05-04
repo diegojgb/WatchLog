e69ff9e9c7eb4c580fa3228cf39bf7feccc0a860
diegojgb <diegojose78@gmail.com>
2024-05-03 23:01:01 -0400

    Removed unnecessary if nesting

--- src/FileWatcher.cpp
+++ src/FileWatcher.cpp
@@ -40,51 +40,51 @@ void FileWatcher::onFileChanged(const QString &path)
 
     Monitor* monitor = m_monitors.get(path);
 
-    if (monitor->m_file.is_open()) {
-        std::string line;
-
-        if (!std::getline(monitor->m_file, line)) { // In case weird modifications were made, and the cursor is broken.
-            emit fileReset();
-            return;
-        }
-
-        do {
-            for (const Notifier* notifier: monitor->m_enabledNotifiers) {
-                if (std::regex_search(line, notifier->regex)) {
-                    if (notifier->toastEnabled())
-                        emit matchFound(notifier->templ);
-                    else {
-                        std::wstring playCommand;
-
-                        if (m_soundsHash.contains(notifier->soundPath())) {
-                            playCommand = m_soundsHash[notifier->soundPath()];
-                        } else {
-                            std::wstring soundAlias = L"NotificationSound" + std::to_wstring(m_soundsHash.size());
-                            std::wstring command = L"open \"" + notifier->soundPath().toStdWString() + L"\" type waveaudio alias " + soundAlias;
-
-                            if (mciSendString(command.c_str(), NULL, 0, NULL) != 0)
-                                Utils::throwError("FileWatcher: Error opening sound file");
-
-                            playCommand = L"play " + soundAlias + L" from 0";
-
-                            m_soundsHash.insert(notifier->soundPath(), playCommand);
-                        }
-
-                        mciSendString(playCommand.c_str(), NULL, 0, NULL);
-                    }
-                    if (!monitor->manyPerUpdate) {
-                        monitor->m_file.seekg(0, std::ios::end);
-                        return;
-                    }
-                }
-            }
-        } while (std::getline(monitor->m_file, line));
-
-        if (!monitor->m_file.eof())
-            Utils::throwError("FileWatcher: error reading file");
-
-        monitor->m_file.clear();
-    } else {
+    if (!monitor->m_file.is_open())
         Utils::throwError("FileWatcher: file's not open");
+
+    std::string line;
+
+    if (!std::getline(monitor->m_file, line)) { // In case weird modifications were made, and the cursor is broken.
+        emit fileReset();
+        return;
     }
+
+    do {
+        for (const Notifier* notifier: monitor->m_enabledNotifiers) {
+            if (!std::regex_search(line, notifier->regex))
+                continue;
+
+            if (notifier->toastEnabled()) {
+                emit matchFound(notifier->templ);
+            } else {
+                std::wstring playCommand;
+
+                if (m_soundsHash.contains(notifier->soundPath())) {
+                    playCommand = m_soundsHash[notifier->soundPath()];
+                } else {
+                    std::wstring soundAlias = L"NotificationSound" + std::to_wstring(m_soundsHash.size());
+                    std::wstring command = L"open \"" + notifier->soundPath().toStdWString() + L"\" type waveaudio alias " + soundAlias;
+
+                    if (mciSendString(command.c_str(), NULL, 0, NULL) != 0)
+                        Utils::throwError("FileWatcher: Error opening sound file");
+
+                    playCommand = L"play " + soundAlias + L" from 0";
+
+                    m_soundsHash.insert(notifier->soundPath(), playCommand);
+                }
+
+                mciSendString(playCommand.c_str(), NULL, 0, NULL);
+            }
+            if (!monitor->manyPerUpdate) {
+                monitor->m_file.seekg(0, std::ios::end);
+                return;
+            }
+        }
+    } while (std::getline(monitor->m_file, line));
+
+    if (!monitor->m_file.eof())
+        Utils::throwError("FileWatcher: error reading file");
+
+    monitor->m_file.clear();
 }
